a504a399fcd1cdbdb00d250de5c87128ce6b2435
darrenjhsu <darrenhsu2015@u.northwestern.edu>
2018-07-09 00:16:52 -0500

    Bug fixes

--- sphere_raster.cu
+++ sphere_raster.cu
@@ -15,8 +15,10 @@ int main () {
         for (int jj = 1; jj <= ii; jj++) {
             if (jj == 1) {
                 w_x = 0.0; w_y = 0.0; w_z = r_iw;
-                if (ii == 1) printf("ii = %d, jj = %d, (%.3f, %.3f, %.3f), core.\n", ii, jj, w_x, w_y, w_z);
-                if (ii > 1) printf("ii = %d, jj = %d, (%.3f, %.3f, %.3f), cap. \n", ii, jj, w_x, w_y, w_z);
+                //if (ii == 1) printf("ii = %d, jj = %d, (%.3f, %.3f, %.3f), core.\n", ii, jj, w_x, w_y, w_z);
+                if (ii == 1) printf("%.3f, %.3f, %.3f, \n", w_x, w_y, w_z);
+                //if (ii > 1) printf("ii = %d, jj = %d, (%.3f, %.3f, %.3f), cap. \n", ii, jj, w_x, w_y, w_z);
+                if (ii > 1) printf("%.3f, %.3f, %.3f, \n", w_x, w_y, w_z);
                 count++;
             } else {
                 b_ij1 = acos((3.0 * (ii - jj) * (ii + jj - 1.0)) / (1.0 + 3.0 * ii * (ii - 1.0)));
@@ -24,27 +26,42 @@ int main () {
                 b_ijw = (b_ij1 + b_ij2) / 2.0;
             }
             for (int kk = 1; kk <= (jj-1)*6; kk++) {
-                a_ijk1 = (2 * kk * PI) / (6 * jj - 1);
-                a_ijk2 = (2 * (kk - 1) * PI) / (6 * jj - 1);
+                a_ijk1 = (2 * kk * PI) / (6 * (jj - 1));
+                a_ijk2 = (2 * (kk - 1) * PI) / (6 * (jj - 1));
                 a_ijkw = (a_ijk1 + a_ijk2) / 2.0;
                 w_x = r_iw * sin(b_ijw) * cos(a_ijkw);
                 w_y = r_iw * sin(b_ijw) * sin(a_ijkw);
                 w_z = r_iw * cos(b_ijw);
-                printf("ii = %d, jj = %d, kk = %d, b_ijw = %.3f, (%.3f, %.3f, %.3f), common. \n", ii, jj, kk, b_ijw, w_x, w_y, w_z);
+                //printf("ii = %d, jj = %d, kk = %d, b_ijw = %.3f, (%.3f, %.3f, %.3f), common. \n", ii, jj, kk, b_ijw, w_x, w_y, w_z);
+                printf("%.3f, %.3f, %.3f, \n", w_x, w_y, w_z);
                 count++;
             }
         }
-        /*for (int jj = 1; jj <= ii; jj++) {
+        for (int jj = 1; jj <= ii; jj++) {
             if (jj == 1) {
-                if (ii == 1) printf("ii = %d, jj = %d, core.\n", ii, -jj);
-                if (ii > 1) printf("ii = %d, jj = %d, cap. \n", ii, -jj);
+                w_x = 0.0; w_y = 0.0; w_z = -r_iw;
+                //if (ii == 1) printf("ii = %d, jj = %d, (%.3f, %.3f, %.3f), core.\n", ii, jj, w_x, w_y, w_z);
+                if (ii == 1) printf("%.3f, %.3f, %.3f, \n", w_x, w_y, w_z);
+                //if (ii > 1) printf("ii = %d, jj = %d, (%.3f, %.3f, %.3f), cap. \n", ii, jj, w_x, w_y, w_z);
+                if (ii > 1) printf("%.3f, %.3f, %.3f, \n", w_x, w_y, w_z);
                 count++;
+            } else {
+                b_ij1 = acos((3.0 * (ii - jj) * (ii + jj - 1.0)) / (1.0 + 3.0 * ii * (ii - 1.0)));
+                b_ij2 = acos((3.0 * (ii - jj + 1.0) * (ii + jj - 2.0)) / (1.0 + 3.0 * ii * (ii - 1.0)));
+                b_ijw = (b_ij1 + b_ij2) / 2.0;
             }
             for (int kk = 1; kk <= (jj-1)*6; kk++) {
-                printf("ii = %d, jj = %d, kk = %d, common. \n", ii, -jj, kk);
+                a_ijk1 = (2 * kk * PI) / (6 * (jj - 1));
+                a_ijk2 = (2 * (kk - 1) * PI) / (6 * (jj - 1));
+                a_ijkw = (a_ijk1 + a_ijk2) / 2.0;
+                w_x = r_iw * sin(b_ijw) * cos(a_ijkw);
+                w_y = r_iw * sin(b_ijw) * sin(a_ijkw);
+                w_z = -r_iw * cos(b_ijw);
+                //printf("ii = %d, jj = %d, kk = %d, b_ijw = %.3f, (%.3f, %.3f, %.3f), common. \n", ii, jj, kk, b_ijw, w_x, w_y, w_z);
+                printf("%.3f, %.3f, %.3f, \n", w_x, w_y, w_z);
                 count++;
             }
-        }*/
+        }
     }
-    printf("count: %d \n", count);
+    //printf("count: %d \n", count);
 }
